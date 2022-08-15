2abde18a0f2279248138d8da10d7dbdc6018efca
Soltvik <106930781+SoltVik@users.noreply.github.com>
2022-08-15 19:59:07 +0200

    changed class name to correct syntax

--- 20220811/src/test/java/TestArray.java
+++ 20220811/src/test/java/TestArray.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class testArray {
+public class TestArray {
     CheckArray checkArray = new CheckArray();
 
     @BeforeEach
