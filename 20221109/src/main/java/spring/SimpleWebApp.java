19937e98effae5943f540e639a3a942417e825bc
Soltvik <106930781+SoltVik@users.noreply.github.com>
2022-11-14 20:14:58 +0100

    Added homework #32

---
+++ 20221109/src/main/java/spring/SimpleWebApp.java
@@ -0,0 +1,19 @@
+package spring;
+
+/**
+ * Java. Homework #32
+ *
+ * @author Viktors Soltums
+ * @version 14 Nov 2022
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApp.class, args);
+	}
+}
